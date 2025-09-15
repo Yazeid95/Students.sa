66bbf3225d7397917a35d89500cb3a46dd2088c1
Yazeid95 <Yazeid95@gmail.com>
2025-09-15 04:59:25 +0300

    Static Update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/Students.sa',
+  assetPrefix: '/Students.sa/',
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
 };
 
 export default nextConfig;
